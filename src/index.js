4d91f9a29017f57822741eb25fe9a2d8de312c30
SHAON <mdshamirulislam@SHAONs-MacBook-Air.local>
2024-06-30 21:00:41 +0600

    [add] init project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import {app} from './app.js'
+import connectDB from "./db/index.js"
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : http://localhost:${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
